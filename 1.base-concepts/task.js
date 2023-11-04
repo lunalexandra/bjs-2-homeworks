90858de8a7718427e4ace26c63e356954791a7dc
Alexandra Luka <lenimide@gmail.com>
2023-11-04 19:47:25 +0500

    First task corrected

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,17 +3,13 @@
 function solveEquation(a, b, c) {
 	let arr = [];
 	let d = (Math.pow(b, 2) - 4 * a * c);
-	if (d < 0) {
-		arr = [];
-	} else if (d === 0) {
+	if (d === 0) {
 		arr = [(-b) / (2 * a)];
 	} else if (d > 0) {
 		arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
 	};
 	return arr;
 }
-
-solveEquation(a, b, c);
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 	let monthlyRate = percent / 100 / 12;
