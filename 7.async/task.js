4b5edb67935e1d9ef67abe833a0b4e0fa51f2ed3
Alexandra Luka <lenimide@gmail.com>
2023-11-28 12:11:39 +0500

    7th task added

---
+++ 7.async/task.js
@@ -0,0 +1,61 @@
+class AlarmClock {
+	constructor(){
+		this.alarmCollection = [];
+		this.intervalId = null;
+	}
+
+	addClock(time, callback) {
+		if (!time || !callback) {
+			throw new Error('Отсутствуют обязательные аргументы');
+		}
+		if (this.alarmCollection.some((item) => item.time === time)) {
+			console.warn('Уже присутствует звонок на это же время');
+			return;
+		}
+		this.alarmCollection.push({
+			time,
+			callback,
+			canCall: true
+		});
+	}
+
+	removeClock(time) {
+		this.alarmCollection = this.alarmCollection.filter((item) => item.time !== time);
+	}
+
+	getCurrentFormattedTime() {
+		let currentTime = new Date();
+		return (`${currentTime.getHours()}:${currentTime.getMinutes()}`);
+	}
+
+	start() {
+		if (this.intervalId) {
+			return;
+		}
+		let checkClock = (item) => {
+			if (item.time === this.getCurrentFormattedTime() && item.canCall) {
+				item.canCall = false;
+				item.callback();
+			}
+		};
+		this.intervalId = setInterval(() => {
+			this.alarmCollection.forEach(checkClock);
+		}, 1000);
+	}
+
+	stop() {
+		clearInterval(this.intervalId);
+		this.intervalId = null;
+	}
+
+	resetAllCalls() {
+		this.alarmCollection.forEach((item) => {
+			item.canCall = true;
+		});
+	}
+
+	clearAlarms() {
+		this.stop();
+		this.alarmCollection = [];
+	}
+}
